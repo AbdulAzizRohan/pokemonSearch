9be1904d3df3d9f4801b9b41cbe07e75a0d88bc7
AbdulAzizRohan <dev.abdulazizrohan@gmail.com>
2025-04-18 14:51:34 +0600

    added variables in js file

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+const creatureName = document.getElementById("creature-name");
+const creatureId = document.getElementById("creature-id");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const types = document.getElementById("types");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defence = document.getElementById("defence");
+const specialAttack = document.getElementById("special-attack");
+const specialDefence = document.getElementById("special-defence");
+const speed = document.getElementById("speed");
